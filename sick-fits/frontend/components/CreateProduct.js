18a5eb1d705cfbd54f1f392361f766b95d465270
Tammy Allen <tjgallen@aol.com>
2021-03-10 17:38:50 -0800

    3-10-21 Pagination

---
+++ sick-fits/frontend/components/CreateProduct.js
@@ -0,0 +1,106 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import DisplayError from './ErrorMessage';
+import useForm from '../lib/useForm';
+import Form from './styles/Form';
+import { ALL_PRODUCTS_QUERY } from './Products';
+import Router from 'next/router';
+
+const CREATE_PRODUCT_MUTATION = gql`
+mutation CREATE_PRODUCT_MUTATION(
+  # Which variables are getting passed in? And what types are they
+  $name: String!
+  $description: String!
+  $price: Int!
+  $image: Upload
+) {
+  createProduct(data: {
+    name: $name
+    description: $description
+    price: $price
+    status: "AVAILABLE"
+    photo: { create: { image: $image, altText: $name } }
+  }
+  ) {
+    id
+    price
+    description
+    name
+  }
+}
+`;
+
+export default function CreateProduct() {
+  const { inputs, handleChange, resetForm, clearForm } = useForm({
+    image: '',
+    name: 'Nice Shoes',
+    price: 34576,
+    description: 'These are the best shoes',
+  });
+  const [createProduct, { loading, error, data }] = useMutation(CREATE_PRODUCT_MUTATION, 
+    {
+    variables: inputs,
+    refetchQueries: [{ query: ALL_PRODUCTS_QUERY}]
+    }
+  );
+  return (
+    <Form 
+      onSubmit={async (e) => {
+        e.preventDefault();
+        // Submit the input fields to the backend;
+        const res = await createProduct();
+        console.log(res);
+        clearForm();
+        // Go to that product's page
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`
+        })
+      }}
+      >
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
+      <label htmlFor="image">
+        Image
+        <input required
+        type="file" 
+        id="image" 
+        name="image" 
+        onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="name">
+        Name
+        <input type="text" 
+        id="name" 
+        name="name" 
+        placeholder="Name"
+        value={inputs.name}
+        onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input 
+        type="number" 
+        id="price" 
+        name="price" 
+        placeholder="Price"
+        value={inputs.price}
+        onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="description">
+        Description
+        <textarea
+        id="description" 
+        name="description" 
+        placeholder="Description"
+        value={inputs.description}
+        onChange={handleChange}
+        />
+      </label>
+      <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
+  )
+}
